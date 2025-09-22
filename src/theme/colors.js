96c893de4bcf299e08d74b525ee0f76a886d088c
Hanzila <Hanzilla@outsourcenz.com>
2025-09-22 19:10:36 +0500

    fix theme error

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -1,18 +1,18 @@
-// const COLORS = {
-//   primary: "#2475FC",
-//   secondary: "#999999",
-//   background: "#F6F6F6",
-//   white: "#FFFFFF",
-//   black: "#000000",
-//   danger: "#FD1207",
-//   borderColor: "#DADADA",
-//   borderColor2: "#C2C2C2",
-//   D9Gray: "#D9D9D9",
-//   lightBlue: "#2473fc10",
-//   green: "#3CCB3C",
-//   F5Gray: "#F5F5F5",
-// };
-// export default COLORS;
+const COLORS = {
+  primary: "#2475FC",
+  secondary: "#999999",
+  background: "#F6F6F6",
+  white: "#FFFFFF",
+  black: "#000000",
+  danger: "#FD1207",
+  borderColor: "#DADADA",
+  borderColor2: "#C2C2C2",
+  D9Gray: "#D9D9D9",
+  lightBlue: "#2473fc10",
+  green: "#3CCB3C",
+  F5Gray: "#F5F5F5",
+};
+export default COLORS;
 
 const lightColors = {
   primary: "#2475FC",
