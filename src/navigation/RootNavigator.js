1061473fab210e1618090789fc2448859b253829
Azaz Mushtaq <aziijnab@gmail.com>
2025-10-03 21:40:26 +0500

    change

--- src/navigation/RootNavigator.js
+++ src/navigation/RootNavigator.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { View, StyleSheet, Platform, StatusBar as RNStatusBar } from "react-native";
 import AuthStack from "./AuthStack";
 import MainTabs from "./MainTabs";
 import { useSelector } from "react-redux";
 import { StatusBar } from "expo-status-bar";
 import COLORS from "../theme/colors";
+import { Platform } from "react-native";
 
 const Stack = createNativeStackNavigator();
 
@@ -14,8 +14,7 @@ export default function RootNavigator() {
 
   return (
     <>
-     <View style={styles.statusBarBg} />
-      <StatusBar style="light" backgroundColor={COLORS.primary} />
+      <StatusBar style={Platform.OS == "ios" ? "dark" : "light"} backgroundColor={COLORS.primary} />
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         {!token ? (
           <Stack.Screen name="Auth" component={AuthStack} />
@@ -26,18 +25,3 @@ export default function RootNavigator() {
     </>
   );
 }
-const STATUSBAR_HEIGHT = Platform.OS === "ios" ? 60 : RNStatusBar.currentHeight || 0;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  statusBarBg: {
-    height: STATUSBAR_HEIGHT,
-    backgroundColor: COLORS.primary, // your desired BG color
-  },
-  content: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
-});
